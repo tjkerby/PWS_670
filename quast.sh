206af83a9f0ffc8fa94c6e8a2f8fd112a6f6fc7b
Emma Wilcox <58602352+emmakw@users.noreply.github.com>
2019-12-19 13:30:57 -0700

    Update quast.sh

--- quast.sh
+++ quast.sh
@@ -4,8 +4,9 @@
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --mem-per-cpu=65536M   # memory per CPU core
-#SBATCH --mail-user=emmakate.wilcox3@gmail.com   # email address
+#SBATCH --mail-user={your email}   # email address
 #SBATCH --mail-type=FAIL
 
+//change filepaths
 
 python /fslhome/emmakw/fsl_groups/fslg_pws670/bin/quast-5.0.2/quast.py -o quast.output -g  /fslhome/emmakw/fsl_groups/fslg_pws670/compute/pws670_emma/annotate/maker/Final_allscaffolds_functional_blast.gff /fslhome/emmakw/fsl_groups/fslg_pws670/compute/pws670_emma/annotate/maker/jelly.out.pilon.fasta.fa
