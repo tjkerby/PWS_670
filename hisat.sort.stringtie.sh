6e602566fcda5eedc421b63cdaf09fe89100262c
Emma Wilcox <58602352+emmakw@users.noreply.github.com>
2019-12-19 13:34:13 -0700

    Update hisat.sort.stringtie.sh

--- hisat.sort.stringtie.sh
+++ hisat.sort.stringtie.sh
@@ -5,22 +5,20 @@
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=4G   # memory per CPU core
 #SBATCH -J "bwa_mem"   # job name
-#SBATCH --mail-user=emmakate.wilcox3@gmail.com   # email address
+#SBATCH --mail-user={your email}   # email address
 #SBATCH --mail-type=FAIL 
 
 module purge                                           
 module load conda-pws
-#module load hisat2
+module load hisat2
 module load conda/bwa_0.7.17
 module load samtools/1.6
 module load stringtie 
 module load cufflinks/v2.2.1
 
-#hisat2 -build -x /fslhome/emmakw/fsl_groups/fslg_pws670/compute/pws670_emma/annotate/jelly.out.pilon.fasta.fasta  hisat.index
-
+hisat2 -build -x /fslhome/emmakw/fsl_groups/fslg_pws670/compute/pws670_emma/annotate/jelly.out.pilon.fasta.fasta  hisat.index
 
 samtools view -Sb --threads $T > $alignments/$name.bam && samtools sort --threads $T -o $alignments/$name.sorted.bam $alignments/$name.bam  && samtools index -@ $T $alignments/$name.sorted.bam
-
 
 stringtie $alignments/$name.sorted.bam  [-l <label>] -o hisat_reference_pilon_jelly_out.gtf -p 8
 
@@ -28,4 +26,4 @@ stringtie --merge $alignments/$name.sorted.bam  [-l <label>] -o hisat_reference_
 
 stringtie -e $alignments/$name.sorted.bam  [-l <label>] -o hisat_reference_pilon_jelly_out.gtf -p 8
 
-cuffdiff -o -b -L -u 
+cuffdiff -o -b -L -u hisat_reference_pilon_jelly_out.gtf
